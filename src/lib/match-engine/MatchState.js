16adc07043dc168832d36900a742ba69f14a584d
NewtalentsG <newtalents403@gmail.com>
2026-09-10 20:52:13 +0200

    Implement MatchState class for match management

---
+++ src/lib/match-engine/MatchState.js
@@ -0,0 +1,56 @@
+export default class MatchState {
+  constructor(homeTeam, awayTeam) {
+    this.status = "not_started";
+
+    this.time = 0;
+
+    this.homeScore = 0;
+    this.awayScore = 0;
+
+    this.homeTeam = homeTeam;
+    this.awayTeam = awayTeam;
+
+    this.players = [];
+
+    this.ball = null;
+
+    this.events = [];
+
+    this.lastEvent = null;
+  }
+
+  addEvent(event) {
+    const completeEvent = {
+      id:
+        Date.now() +
+        Math.random(),
+
+      minute: Math.floor(
+        this.time / 60
+      ),
+
+      ...event,
+    };
+
+    this.events.push(
+      completeEvent
+    );
+
+    this.lastEvent =
+      completeEvent;
+
+    if (this.events.length > 100) {
+      this.events.shift();
+    }
+  }
+
+  getScore() {
+    return `${this.homeScore} - ${this.awayScore}`;
+  }
+
+  getMinute() {
+    return Math.floor(
+      this.time / 60
+    );
+  }
+}
