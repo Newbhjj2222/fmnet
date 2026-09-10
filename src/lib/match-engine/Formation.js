970645ab2014d9615b9d8684f6842b4fcf6106f4
NewtalentsG <newtalents403@gmail.com>
2026-09-10 21:46:06 +0200

    Refactor Formation.js for conciseness

--- src/lib/match-engine/Formation.js
+++ src/lib/match-engine/Formation.js
@@ -1,63 +1,18 @@
-import {
-  FORMATIONS,
-  PITCH,
-} from "./constants";
+import { FORMATIONS, PITCH } from "./constants";
 
-export function normalizeFormation(
-  formation
-) {
-  if (
-    FORMATIONS[formation]
-  ) {
-    return formation;
-  }
-
-  return "4-4-2";
+export function normalizeFormation(formation) {
+  return FORMATIONS[formation] ? formation : "4-4-2";
 }
 
-export function getFormation(
-  formation
-) {
-  return (
-    FORMATIONS[
-      normalizeFormation(
-        formation
-      )
-    ]
-  );
+export function getFormation(formation) {
+  return FORMATIONS[normalizeFormation(formation)];
 }
 
-export function getFormationPosition(
-  formation,
-  index,
-  side
-) {
-  const points =
-    getFormation(
-      formation
-    );
-
-  const point =
-    points[index] ||
-    points[0];
-
-  let x =
-    point[1] *
-    PITCH.width;
-
-  let y =
-    point[2] *
-    PITCH.height;
-
-  if (side === "away") {
-    x =
-      PITCH.width -
-      x;
-  }
-
-  return {
-    role: point[0],
-    x,
-    y,
-  };
+export function getFormationPosition(formation, index, side) {
+  const points = getFormation(formation);
+  const point = points[index] || points[0];
+  let x = point[1] * PITCH.width;
+  let y = point[2] * PITCH.height;
+  if (side === "away") x = PITCH.width - x;
+  return { role: point[0], x, y };
 }
